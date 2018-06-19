30b7a5a68073799f890a6623c999647e0a735581
fumiya kobayashi <s1642047ee@s.chibakoudai.jp>
2018-06-19 18:17:56 +0900

    c1

--- sample/index.php
+++ sample/index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>ろくまる農園</title>
+        <title>ろくまる農園だよ</title>
     </head>
     <body>
         <?php
