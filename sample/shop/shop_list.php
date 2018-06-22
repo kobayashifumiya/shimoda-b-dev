0411b31d5b6f63103f11cb2288340b4cbe3ffa82
SSK <s1642057rx@s.chuibakoudai.jp>
2018-06-22 14:09:49 +0900

    shop search

--- sample/shop/shop_list.php
+++ sample/shop/shop_list.php
@@ -1,32 +1,12 @@
-<?php
-session_start();
-session_regenerate_id(true);
-if(isset($_SESSION['member_login'])==false)
-{
-	print 'ようこそゲスト様　';
-	print '<a href="member_login.html">会員ログイン</a><br />';
-	print '<br />';
-}
-else
-{
-	print 'ようこそ';
-	print $_SESSION['member_name'];
-	print ' 様　';
-	print '<a href="member_logout.php">ログアウト</a><br />';
-	print '<br />';
-}
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>ろくまる農園</title>
-</head>
-<body>
-
+<!DOCTYPE html> 
+<html> 
+<head> 
+<meta charset="UTF-8"> 
+<title>ろくまる農園</title> 
+</head> 
+<body> 
 <?php require_once('../common/common.php'); ?>
-<form method="post" action="shop_search.php"> 
+<form method="post" action="shop_list.php"> 
 食材
 <?php pulldown_syokuzai(); ?>
 産地
@@ -38,52 +18,78 @@ else
 <p><input type="submit" value="検索"></p>
 </form>
 
+<?php 
 
 
-<?php
+try{
 
-try
-{
 
-$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
-$user='root';
-$password='';
-$dbh=new PDO($dsn,$user,$password);
-$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-$sql='SELECT code,name,price FROM mst_product WHERE 1';
-$stmt=$dbh->prepare($sql);
-$stmt->execute();
-
-$dbh=null;
-
-print '商品一覧<br /><br />';
-
-while(true)
-{
-	$rec=$stmt->fetch(PDO::FETCH_ASSOC);
-	if($rec==false)
-	{
-		break;
-	}
-	print '<a href="shop_product.php?procode='.$rec['code'].'">';
-	print $rec['name'].'---';
-	print $rec['price'].'円';
-	print '</a>';
-	print '<br />';
-}
-
-print '<br />';
-print '<a href="shop_cartlook.php">カートを見る</a><br />';
-
-}
-catch (Exception $e)
-{
-	 print 'ただいま障害により大変ご迷惑をお掛けしております。';
-	 exit();
-}
-
-?>
-
+  require_once('../common/common.php');
+  
+  $dsn='mysql:dbname=ensyu;host=localhost;charset=utf8';;
+  $user='root';
+  $password='';
+  $dbh=new PDO($dsn,$user,$password);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+  
+  $sql='SELECT code,name,price,syokuzai,santi,kakaku FROM mst_product WHERE 1';
+  $stmt=$dbh->prepare($sql);
+  $stmt->execute();
+  
+  
+  $dbh=null;
+  
+  print '検索結果<br /><br />'; 
+  
+  if($sql!==0){ 
+    while(true) 
+    { 
+     $rec=$stmt->fetch(PDO::FETCH_ASSOC); 
+     if($rec==false) 
+     { 
+      break; 
+     }  
+     if(isset($_POST['keyword']) && $_POST['keyword'] != '')
+  {
+    $keyword = $_POST['keyword'];
+    $syokuzai=$_POST['syokuzai'];
+    $santi=$_POST['santi'];
+    $kakaku=$_POST['kakaku'];
+  
+      if((strpos($rec['syokuzai'],$syokuzai)!==false) 
+      &&(strpos($rec['santi'],$santi)!==false) 
+      &&(strpos($rec['kakaku'],$kakaku)!==false)
+      &&(strpos($rec['name'],$keyword)!==false) ) 
+      { 
+     print '<a href="shop_product.php?procode='.$rec['code'].'">'; 
+     print $rec['name'].'---'; 
+     print $rec['price'].'円'; 
+     print '</a>'; 
+     print '<br />'; 
+     }
+     } else { 
+     print '<a href="shop_product.php?procode='.$rec['code'].'">'; 
+     print $rec['name'].'---'; 
+     print $rec['price'].'円'; 
+     print '</a>'; 
+     print '<br />'; 
+     } 
+    } 
+    }
+  
+  print '<br />'; 
+  print '<a href="shop_cartlook.php">カートを見る</a><br />'; 
+  
+  } 
+  catch (Exception $e) 
+  { 
+    print 'ただいま障害により大変ご迷惑をお掛けしております。'; 
+    exit(); 
+  } 
+  
+  ?>
+  
+  
 </body>
 </html>
+  
