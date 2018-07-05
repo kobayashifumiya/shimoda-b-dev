6eff1566ab8c1439e9f7b99fc63ee7a888c8213d
kashiwairei <s1642037PP@s.chibakoudai.jp>
2018-07-05 23:57:50 +0900

    Revert "Merge branch 'master' of https://github.com/kobayashifumiya/shimoda-b-dev"
    This reverts commit 37e26ede7d84cee2f1d52e018c8a3e14dc194515, reversing
    changes made to 5f0e16a8ec4a27cace6ff74222d960f2b75c04ae.

--- sample/shop/shop_list.php
+++ sample/shop/shop_list.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 session_regenerate_id(true);
@@ -51,32 +52,20 @@ try
 //検索したことがない場合
 	if(isset($_POST['search'])==false)
 	{
-
-
+//$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+/*$dsn='mysql:dbname=shop;host=localhost;charset=utf8';*/
 $dsn='mysql:dbname=ensyu;host=localhost;charset=utf8';
 $user='root';
 $password='';
 $dbh=new PDO($dsn,$user,$password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-$sql='SELECT code,name,price,gazou FROM mst_product WHERE 1';
+$sql='SELECT code,name,price FROM mst_product WHERE 1';
 $stmt=$dbh->prepare($sql);
 $stmt->execute();
 
-$rec=$stmt->fetch(PDO::FETCH_ASSOC);
-$pro_gazou_name=$rec['gazou'];
-
 $dbh=null;
 
-
-if($pro_gazou_name=='')
-{
- $disp_gazou='';
-}
-else
-{
- $disp_gazou='<img src="../product/gazou/'.$pro_gazou_name.'">';
-}
 
 
 while(true)
@@ -87,7 +76,6 @@ while(true)
 		break;
 	}
 	print '<a href="shop_product.php?procode='.$rec['code'].'">';
-	print $disp_gazou.'<br />'; 
 	print $rec['name'].'─';
 	print $rec['price'].'円';
 	print '</a>';
@@ -114,28 +102,16 @@ $password='';
 $dbh=new PDO($dsn,$user,$password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-$sql='SELECT code,name,price,gazou,syokuzai,santi,kakaku
+$sql='SELECT code,name,price,syokuzai,santi,kakaku
 FROM mst_product WHERE 1';
 
 
 $stmt=$dbh->prepare($sql);
 $stmt->execute();
 
-$rec=$stmt->fetch(PDO::FETCH_ASSOC);
-$pro_gazou_name=$rec['gazou'];
-
 $dbh=null;
 
 $_x=0;
-
-if($pro_gazou_name=='')
-{
- $disp_gazou='';
-}
-else
-{
- $disp_gazou='<img src="../product/gazou/'.$pro_gazou_name.'">';
-}
 
 
 
@@ -156,7 +132,6 @@ if($keyword==''){
 		&&(strpos($rec['kakaku'],$kakaku)!==false))
 		{
 		print '<a href="shop_product.php?procode='.$rec['code'].'">';
-		print $disp_gazou.'<br />'; 
 		print $rec['name'].'─';
 		print $rec['price'].'円';
 		print '</a>';
@@ -184,7 +159,6 @@ while(true)
 	&&(strpos($rec['name'],$keyword)!==false))
 	{
 	print '<a href="shop_product.php?procode='.$rec['code'].'">';
-	print $disp_gazou.'<br />'; 
 	print $rec['name'].'─';
 	print $rec['price'].'円';
 	print '</a>';
