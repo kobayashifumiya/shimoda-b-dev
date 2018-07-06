bcb0561d62556f4d25d0e06509b047dae4cc2714
fumiya kobayashi <s1642047ee@s.chibakoudai.jp>
2018-07-06 21:34:50 +0900

    disp

--- sample/order/order_disp.php
+++ sample/order/order_disp.php
@@ -8,8 +8,7 @@
     <body>
     注文番号入力画面<br /><br />
     管理者専用ページ<br /><br />
-
-
+<div style="text-align:center">
 <?php
 
 try
@@ -43,18 +42,24 @@ while(true)
 		break;
 	}
     if($code==$rec['order_number']){
+        print "注文番号<br/>";
         print $rec['order_number'];
-        print '<br/>';
+        print '<br/><br/>';
+        print "注文日時<br/>";
         print $rec['order_date_and_time'];
-        print '<br/>';
+        print '<br/><br/>';
+        print "会員ID<br/>";
         print $rec['id'];
-        print '<br/>';
+        print '<br/><br/>';
+        print "商品名<br/>";
         print $rec['product_name'];
-        print '<br/>';
+        print '<br/><br/>';
+        print "合計金額<br/>";
         print $rec['total_fee'];
-        print '<br/>';
+        print '<br/><br/>';
+        print "購入個数<br/>";
         print $rec['purchase_number'];
-        print '<br/>';
+        print '<br/><br/>';
     }
 }
 
@@ -77,27 +82,28 @@ while(true)
      exit();
  }
 
+ 
 
-?>
 
-<table border="1" table cellpadding="10" table cellspacing>
-<caption>発注商品情報</caption>
-<tr>
-<th>注文番号</th> <th>注文日</th> <th>ユーザーID</th> <th>商品名</th> <th>合計金額</th> <th>購入個数</th>
-</tr>
-<tr>
-<td>00000001</td> <td>2018/5/20</td> <td>1</td> <td>桃太郎(トマト,北海道)</td> <td>300円</td> <td>3個</td>
-</tr>
-</td>
-</table>
-<form method="POST"
-      action="mailsend.php">
-      
-    <input type="submit" name="submit" value="メールを送信">
+    //<table border="1" table cellpadding="10" table cellspacing>
+//<caption>発注商品情報</caption>
+//<tr>
+//<th>注文番号</th> <th>注文日</th> <th>ユーザーID</th> <th>商品名</th> <th>合計金額</th> <th>購入個数</th>
+//</tr>
+//<tr>
+//<td>00000001</td> <td>2018/5/20</td> <td>1</td> <td>桃太郎(トマト,北海道)</td> <td>300円</td> <td>3個</td>
+//</tr>
+//</td>
+//</table>
     
+    ?>
+    <form method="POST"
+     action="mailsend.php">
+          <input type="submit" name="submit" value="メールを送信">
 
 </form>
   <br />
+  </div>
  
   </body>  
 </html>
